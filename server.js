c4ece263e379b3c98d6a7dca175a6764d6ed1f03
guy.thanet <kayguyx2@gmail.com>
2023-04-28 19:42:32 +0700

    ssss

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get("/", (req, res) => {
         .set({ hour: 23, minute: 59, second: 0, millisecond: 0 })
         .tz("Asia/Bangkok")
         .utcOffset("+0700"),
+      s2: momentTz().endOf("day").tz("Asia/Bangkok").utcOffset("+0700"),
     },
   });
 });
